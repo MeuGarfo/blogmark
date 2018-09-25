d7d38807d9682b4af0790db04abe3c2d229e8c35
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-24 23:39:12 -0300

    R  asset/script.js -> asset/script1.js R  asset/style.css -> asset/style1.css M  template.php

--- template.php
+++ template.php
@@ -3,8 +3,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title><?php print @$title; ?></title>
-<script src="asset/script.js"></script>
-<link rel="stylesheet" href="asset/style.css">
+<script src="asset/script1.js"></script>
+<link rel="stylesheet" href="asset/style1.css">
 <link rel="shortcut icon" href="asset/logo180.png" type="image/x-icon" />
 </head>
 <body>
