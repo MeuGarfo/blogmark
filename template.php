27137639e53f6a53e8620189c6cfe2cf789f03e4
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-26 23:46:33 -0300

    R  asset/style9.css -> asset/style10.css M  template.php

--- template.php
+++ template.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title><?php print @$title; ?></title>
 <script src="asset/script1.js"></script>
-<link rel="stylesheet" href="asset/style9.css">
+<link rel="stylesheet" href="asset/style10.css">
 <link rel="shortcut icon" href="asset/logo180.png" type="image/x-icon" />
 </head>
 <body>
