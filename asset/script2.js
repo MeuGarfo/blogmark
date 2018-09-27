a50fc8ea12487ad8c230d3e55f19db5cabe7a537
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-27 20:54:04 -0300

    R  asset/script1.js -> asset/script2.js M  template.php

--- asset/script2.js
+++ asset/script2.js
@@ -1,5 +1,6 @@
-var before_loadtime = new Date().getTime();
-window.onload = Pageloadtime;
+//var before_loadtime = new Date().getTime();
+var before_loadtime = performance.timing.connectStart;
+
 function Pageloadtime() {
     var aftr_loadtime = new Date().getTime();
     pgloadtime = (aftr_loadtime - before_loadtime) / 1000
@@ -8,3 +9,6 @@ function Pageloadtime() {
     var msgHtml="<center><small>Página carregada em " + pgloadtime + " segundos</small></center>";
     bodyElm.innerHTML = bodyHtml+msgHtml;
 }
+
+//window.onload = Pageloadtime;
+window.addEventListener("load", Pageloadtime);
