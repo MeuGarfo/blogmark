38cb978f180252f99569bc22dc743881da510c60
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-27 21:32:13 -0300

    D  asset/script3.js A  asset/script4.js M  template.php

--- template.php
+++ template.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title><?php print @$title; ?></title>
-<script src="asset/script3.js"></script>
+<script src="asset/script4.js"></script>
 <link rel="stylesheet" href="asset/style10.css">
 <link rel="shortcut icon" href="asset/logo180.png" type="image/x-icon" />
 </head>
